59ee041c3a4dd838753b11454c2a5e522f464af5
Ryan McCollam <Ryan@Ryans-MacBook-Pro.local>
2015-04-08 11:49:55 -0500

    Initial Commit

---
+++ JoggingCircuit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JoggingCircuit
+//
+//  Created by Ryan McCollam on 4/8/15.
+//  Copyright (c) 2015 Ryan McCollam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
